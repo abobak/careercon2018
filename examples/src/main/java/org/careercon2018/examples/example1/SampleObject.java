918098d0f6d2153b575643ef9871f34901f5a63d
Andrzej Bobak <a.bobak@interia.pl>
2018-04-22 00:32:56 +0200

    Example 1 commit

--- examples/src/main/java/org/careercon2018/examples/example1/SampleObject.java
+++ examples/src/main/java/org/careercon2018/examples/example1/SampleObject.java
@@ -1,9 +1,26 @@
 package org.careercon2018.examples.example1;
 
+import java.util.LinkedList;
 import java.util.List;
 
+import lombok.Getter;
+
+@Getter
 public class SampleObject {
 	Integer squareInput;
 	List<Integer> numbers;
 	String name;
+	
+	public SampleObject(Integer input) {
+		this.squareInput = input * input;
+		this.name = input.toString();
+		numbers = new LinkedList<>();
+		for (int i=0; i < input.intValue() - 1 ;i++) {
+			numbers.add(i);
+		}
+	}
+	
+	public int getNumbersSize() {
+		return this.numbers.size();
+	}
 }
